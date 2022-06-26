6e86dabe9902eafeeabd0d62feb621aacaf00155
Vidhi Sharma <vidhi.sharma@salesforce.com>
2022-06-26 17:23:43 +0530

    updated visualize method

--- data_visualization.py
+++ data_visualization.py
@@ -3,20 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Visualization:
-    def __init__(self, data, plotType):
+    def __init__(self, data):
         self.data = data
-        self.plotType = plotType
 
-    def visualize(self, X= None, Y=None, category=None):
-        if(self.plotType == "Bar Plot"):
+    def visualize(self, plot_type, X= None, Y=None, category=None):
+        if(plot_type == "Bar Plot"):
             self.barPlot_visualize(X, Y)
-        elif(self.plotType == "Line Plot"):
+        elif(plot_type == "Line Plot"):
             self.linePlot_visualize(X, Y, category)
-        elif(self.plotType == "Scatter Plot"):
+        elif(plot_type == "Scatter Plot"):
             self.scatterPlot_visualize(X, Y, category)
-        elif(self.plotType == "Heat Map"):
+        elif(plot_type == "Heat Map"):
             self.heatMap_visualize()
-        elif(self.plotType == "Violin Plot"):
+        elif(plot_type == "Violin Plot"):
             self.violinPlot_visualize(X, Y)
         else:
             print("Plot is not supported. Supported Plots: Bar Plot, Line Plot, Scatter Plot, Heat Map, Scatter Plot")
@@ -25,6 +24,7 @@ class Visualization:
         self.setup()
         dataset= self.data
         p= sns.barplot(x = X, y = Y, data = dataset)
+        p.set_title("Bar Plot", fontsize = 70)
         hatches = ['-', '+', 'x', '\\', '*', 'o']
         for i,thisbar in enumerate(p.patches):
             thisbar.set_hatch(hatches[i])
@@ -36,6 +36,7 @@ class Visualization:
         self.setup()
         dataset = self.data
         p= sns.lineplot(x=X, y=Y, hue=category, style=category, data=dataset, markers=True, markersize=30)
+        p.set_title("Line Plot", fontsize = 70)
         self.show(p, X, Y)
 
     def scatterPlot_visualize(self, X, Y, category):
@@ -43,6 +44,7 @@ class Visualization:
         color= sns.color_palette("colorblind", as_cmap= True)
         dataset = self.data
         p= sns.scatterplot(x= X, y=Y,data=dataset, hue=category, style=category, s=300, cmap= color)
+        p.set_title("Scatter Plot", fontsize = 70)
         self.show(p, X, Y)
 
     def heatMap_visualize(self):
@@ -53,6 +55,7 @@ class Visualization:
         linecolor = "yellow"
         corrMatrix = dataset.corr()
         p= sns.heatmap(corrMatrix,linewidths=linewidths,linecolor=linecolor, cmap= color, annot=True)
+        p.set_title("Heat Map", fontsize = 70)
         p.set_title(self.plotType, fontsize = 70)
         plt.legend(markerscale=5)
         plt.show()
@@ -62,6 +65,7 @@ class Visualization:
         color= sns.color_palette("colorblind", as_cmap= True)
         dataset = self.data  
         p= sns.violinplot(x = X, y = Y, data = dataset, cmap= color)
+        p.set_title("Violin Plot", fontsize = 70)
         self.show(p, X, Y)
         
     def setup(self):
@@ -74,6 +78,5 @@ class Visualization:
     def show(self, p, X, Y):
         p.set_xlabel(X, fontsize = 60)
         p.set_ylabel(Y, fontsize = 60)
-        p.set_title(self.plotType, fontsize = 70)
         plt.legend(markerscale=4)
         plt.show()
